9e1339a56c5e8df0a4569546294b47792d84f986
AllJrLwrs <aldibayusp12@gmail.com>
2025-01-15 17:22:12 +0700

    Update send_token.py

--- send_token.py
+++ send_token.py
@@ -10,7 +10,7 @@ from datetime import datetime, timezone
 from telegram import Bot
 
 # Fungsi untuk membuat token acak
-def generate_random_token(length=30):
+def generate_random_token(length=50):
     characters = string.ascii_letters + string.digits  # Kombinasi huruf dan angka
     return ''.join(random.choice(characters) for _ in range(length))
 
@@ -84,7 +84,7 @@ async def send_and_pin_token():
 
 _Daily Token is Appearing Now_
 
-> ||***     {random_token}     ***||
+> ||*** {random_token} ***||
 
 ***info     : Use this Token to enter the Script***
 ***Expire : this token is only valid for 3 hours***
